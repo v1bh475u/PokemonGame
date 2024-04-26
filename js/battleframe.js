3cb0a50ec0b498ef0760161192895aa2b3c3667e
v1bh475u <maulikbarot2915@gmail.com>
2024-04-26 17:20:22 +0530

    More bug fixes

--- js/battleframe.js
+++ js/battleframe.js
@@ -5,20 +5,376 @@ let playermoves = {};
 let enemymoves = {};
 let playermon = {};
 let enemymon = {};
-let typelookup = new Promise((resolve, reject) => {
-    fetch('./JSON/typelookup.json').then(response => {
-        resolve(response.json());
-    }).catch(err => {
-        reject(err)
-    })
-});
+let typelookup = {
+    "normal": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 0.5,
+        "ghost": 0,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "fire": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 0.5,
+        "electric": 1,
+        "grass": 2,
+        "ice": 2,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 2,
+        "rock": 0.5,
+        "ghost": 1,
+        "dragon": 0.5,
+        "dark": 1,
+        "steel": 2,
+        "fairy": 1
+    },
+    "water": {
+        "normal": 1,
+        "fire": 2,
+        "water": 0.5,
+        "electric": 1,
+        "grass": 0.5,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 2,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 2,
+        "ghost": 1,
+        "dragon": 0.5,
+        "dark": 1,
+        "steel": 1,
+        "fairy": 1
+    },
+    "electric": {
+        "normal": 1,
+        "fire": 1,
+        "water": 2,
+        "electric": 0.5,
+        "grass": 0.5,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 0,
+        "flying": 2,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 0.5,
+        "dark": 1,
+        "steel": 1,
+        "fairy": 1
+    },
+    "grass": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 2,
+        "electric": 1,
+        "grass": 0.5,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 0.5,
+        "ground": 2,
+        "flying": 0.5,
+        "psychic": 1,
+        "bug": 0.5,
+        "rock": 2,
+        "ghost": 1,
+        "dragon": 0.5,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "ice": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 0.5,
+        "electric": 1,
+        "grass": 2,
+        "ice": 0.5,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 2,
+        "flying": 2,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 2,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "fighting": {
+        "normal": 2,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 2,
+        "fighting": 1,
+        "poison": 0.5,
+        "ground": 1,
+        "flying": 0.5,
+        "psychic": 0.5,
+        "bug": 0.5,
+        "rock": 2,
+        "ghost": 0,
+        "dragon": 1,
+        "dark": 2,
+        "steel": 2,
+        "fairy": 0.5
+    },
+    "poison": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 2,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 0.5,
+        "ground": 0.5,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 0.5,
+        "ghost": 0.5,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 0,
+        "fairy": 2
+    },
+    "ground": {
+        "normal": 1,
+        "fire": 2,
+        "water": 1,
+        "electric": 2,
+        "grass": 0.5,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 2,
+        "ground": 1,
+        "flying": 0,
+        "psychic": 1,
+        "bug": 0.5,
+        "rock": 2,
+        "ghost": 1,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 2,
+        "fairy": 1
+    },
+    "flying": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 0.5,
+        "grass": 2,
+        "ice": 1,
+        "fighting": 2,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 2,
+        "rock": 0.5,
+        "ghost": 1,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "psychic": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 2,
+        "poison": 2,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 0.5,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 1,
+        "dark": 0,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "bug": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 1,
+        "electric": 1,
+        "grass": 2,
+        "ice": 1,
+        "fighting": 0.5,
+        "poison": 0.5,
+        "ground": 1,
+        "flying": 0.5,
+        "psychic": 2,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 0.5,
+        "dragon": 1,
+        "dark": 2,
+        "steel": 0.5,
+        "fairy": 0.5
+    },
+    "rock": {
+        "normal": 1,
+        "fire": 2,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 2,
+        "fighting": 0.5,
+        "poison": 1,
+        "ground": 0.5,
+        "flying": 2,
+        "psychic": 1,
+        "bug": 2,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "ghost": {
+        "normal": 0,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 2,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 2,
+        "dragon": 1,
+        "dark": 0.5,
+        "steel": 0.5,
+        "fairy": 1
+    },
+    "dragon": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 2,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 0
+    },
+    "dark": {
+        "normal": 1,
+        "fire": 1,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 0.5,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 2,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 2,
+        "dragon": 1,
+        "dark": 0.5,
+        "steel": 0.5,
+        "fairy": 0.5
+    },
+    "steel": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 0.5,
+        "electric": 0.5,
+        "grass": 1,
+        "ice": 2,
+        "fighting": 1,
+        "poison": 1,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 2,
+        "ghost": 1,
+        "dragon": 1,
+        "dark": 1,
+        "steel": 0.5,
+        "fairy": 2
+    },
+    "fairy": {
+        "normal": 1,
+        "fire": 0.5,
+        "water": 1,
+        "electric": 1,
+        "grass": 1,
+        "ice": 1,
+        "fighting": 2,
+        "poison": 0.5,
+        "ground": 1,
+        "flying": 1,
+        "psychic": 1,
+        "bug": 1,
+        "rock": 1,
+        "ghost": 1,
+        "dragon": 2,
+        "dark": 2,
+        "steel": 0.5,
+        "fairy": 1
+    }
+};
 
 async function getPokemonmoves(pokemon) {
     let moves = pokemon['moves'];
     let chosen_moves = [];
     let i = 0;
     while (i < 4) {
-        let x = Math.floor(Math.random() * Object.getOwnPropertyNames(moves).length) % Object.getOwnPropertyNames(moves).length;
+        let x = Object.getOwnPropertyNames(moves).length;
+        while (x >= Object.getOwnPropertyNames(moves).length) x = Math.floor(Math.random() * Object.getOwnPropertyNames(moves).length) % Object.getOwnPropertyNames(moves).length;
         let chosen_move = {};
         if (!(chosen_moves.hasOwnProperty(moves[x]['move']['name']))) {
             chosen_move['name'] = moves[x]['move']['name'];
@@ -200,7 +556,7 @@ function turn(player_move, enemy_moves, playermon, enemymon) {
 async function enemnymonchoice() {
     enemymoves = {};
     let x = 0;
-    while (enemymon['battle_stats']['hp'] <= 0) {
+    while (enemyteam[x]['battle_stats']['hp'] <= 0) {
         x = Math.floor(Math.random() * 6) % 6;
     }
     let front = enemyteam[x];
@@ -437,6 +793,8 @@ async function switchmon(index) {
             playermoves = await getPokemonmoves(playermon);
         else
             playermoves = playermon.battle_moves;
+        playermon = playerteam[index];
+        document.getElementById('player_hp_bar').remove();
         let hp_bar = document.createElement('div');
         hp_bar.style.width = (playermon.battle_stats.hp / playermon.battle_stats.max_hp).toString() + '%';
         hp_bar.setAttribute('class', 'hp-bar');
@@ -463,5 +821,6 @@ function updateBackgroundColor() {
     var red = 255 - (percentage * 255 / 100);
     var green = percentage * 255 / 100;
     myDiv.style.backgroundColor = 'rgb(' + red.toString() + ', ' + green.toString() + ', 0)';
-}
-gameloop();
+};
+
+window.onload = () => { gameloop() };
